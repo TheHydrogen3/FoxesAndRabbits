9aa631a5c5b16e83d40f8380d27d928bed54a350
TheHydrogen3 <hallvarbjordalsbakke@hotmail.com>
2017-02-08 09:07:19 +0100

    Add files via upload

---
+++ FoxesAndRabbitsSim/src/foxesandrabbitssim/FoxesAndRabbitsSim.java
@@ -0,0 +1,18 @@
+
+package foxesandrabbitssim;
+
+/**
+ *
+ * @author HaIIvard
+ */
+public class FoxesAndRabbitsSim
+{
+    
+
+    public static void main(String[] args)
+    {
+        Simulator sim = new Simulator();
+        sim.runLongSimulation();
+    }
+    
+}
