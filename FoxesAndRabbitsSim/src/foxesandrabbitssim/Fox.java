f107481485bf27320aabd92f7dc1a1b83436286a
HaIIvard <HaIIvard@158.38.193.235>
2017-02-09 10:53:48 +0100

    Added gender to actor and rabbit

--- FoxesAndRabbitsSim/src/foxesandrabbitssim/Fox.java
+++ FoxesAndRabbitsSim/src/foxesandrabbitssim/Fox.java
@@ -10,16 +10,15 @@ import java.util.Iterator;
 import java.util.Random;
 
 /**
- * A simple model of a fox.
- * Foxes age, move, eat rabbits, and die.
- * 
+ * A simple model of a fox. Foxes age, move, eat rabbits, and die.
+ *
  * @author David J. Barnes and Michael Kölling
  * @version 2011.07.31
  */
 public class Fox extends Actor
 {
     // Characteristics shared by all foxes (class variables).
-    
+
     // The age at which a fox can start to breed.
     private static final int BREEDING_AGE = 15;
     // The age to which a fox can live.
@@ -33,7 +32,7 @@ public class Fox extends Actor
     private static final int RABBIT_FOOD_VALUE = 9;
     // A shared random number generator to control breeding.
     private static final Random rand = Randomizer.getRandom();
-    
+
     // Individual characteristics (instance fields).
     // The fox's age.
     private int age;
@@ -41,9 +40,9 @@ public class Fox extends Actor
     private int foodLevel;
 
     /**
-     * Create a fox. A fox can be created as a new born (age zero
-     * and not hungry) or with a random age and food level.
-     * 
+     * Create a fox. A fox can be created as a new born (age zero and not
+     * hungry) or with a random age and food level.
+     *
      * @param randomAge If true, the fox will have random age and hunger level.
      * @param field The field currently occupied.
      * @param location The location within the field.
@@ -51,20 +50,21 @@ public class Fox extends Actor
     public Fox(boolean randomAge, Field field, Location location)
     {
         super(field, location);
-        if(randomAge) {
+        if (randomAge)
+        {
             age = rand.nextInt(MAX_AGE);
             foodLevel = rand.nextInt(RABBIT_FOOD_VALUE);
-        }
-        else {
+        } else
+        {
             age = 0;
             foodLevel = RABBIT_FOOD_VALUE;
         }
     }
-    
+
     /**
-     * This is what the fox does most of the time: it hunts for
-     * rabbits. In the process, it might breed, die of hunger,
-     * or die of old age.
+     * This is what the fox does most of the time: it hunts for rabbits. In the
+     * process, it might breed, die of hunger, or die of old age.
+     *
      * @param field The field currently occupied.
      * @param newFoxes A list to return newly born foxes.
      */
@@ -72,19 +72,26 @@ public class Fox extends Actor
     {
         incrementAge();
         incrementHunger();
-        if(isAlive()) {
-            giveBirth(newFoxes);            
+        if (isAlive())
+        {
+            if (getGender().equals("Female"))
+
+            {
+                giveBirth(newFoxes);
+            }
             // Move towards a source of food if found.
             Location newLocation = findFood();
-            if(newLocation == null) { 
+            if (newLocation == null)
+            {
                 // No food found - try to move to a free location.
                 newLocation = getField().freeAdjacentLocation(getLocation());
             }
             // See if it was possible to move.
-            if(newLocation != null) {
+            if (newLocation != null)
+            {
                 setLocation(newLocation);
-            }
-            else {
+            } else
+            {
                 // Overcrowding.
                 setDead();
             }
@@ -97,25 +104,28 @@ public class Fox extends Actor
     private void incrementAge()
     {
         age++;
-        if(age > MAX_AGE) {
+        if (age > MAX_AGE)
+        {
             setDead();
         }
     }
-    
+
     /**
      * Make this fox more hungry. This could result in the fox's death.
      */
     private void incrementHunger()
     {
         foodLevel--;
-        if(foodLevel <= 0) {
+        if (foodLevel <= 0)
+        {
             setDead();
         }
     }
-    
+
     /**
-     * Look for rabbits adjacent to the current location.
-     * Only the first live rabbit is eaten.
+     * Look for rabbits adjacent to the current location. Only the first live
+     * rabbit is eaten.
+     *
      * @return Where food was found, or null if it wasn't.
      */
     private Location findFood()
@@ -123,12 +133,15 @@ public class Fox extends Actor
         Field field = getField();
         List<Location> adjacent = field.adjacentLocations(getLocation());
         Iterator<Location> it = adjacent.iterator();
-        while(it.hasNext()) {
+        while (it.hasNext())
+        {
             Location where = it.next();
             Object animal = field.getObjectAt(where);
-            if(animal instanceof Rabbit) {
+            if (animal instanceof Rabbit)
+            {
                 Rabbit rabbit = (Rabbit) animal;
-                if(rabbit.isAlive()) { 
+                if (rabbit.isAlive())
+                {
                     rabbit.setDead();
                     foodLevel = RABBIT_FOOD_VALUE;
                     return where;
@@ -137,10 +150,11 @@ public class Fox extends Actor
         }
         return null;
     }
-    
+
     /**
-     * Check whether or not this fox is to give birth at this step.
-     * New births will be made into free adjacent locations.
+     * Check whether or not this fox is to give birth at this step. New births
+     * will be made into free adjacent locations.
+     *
      * @param newFoxes A list to return newly born foxes.
      */
     private void giveBirth(List<Actor> newFoxes)
@@ -150,22 +164,24 @@ public class Fox extends Actor
         Field field = getField();
         List<Location> free = field.getFreeAdjacentLocations(getLocation());
         int births = breed();
-        for(int b = 0; b < births && free.size() > 0; b++) {
+        for (int b = 0; b < births && free.size() > 0; b++)
+        {
             Location loc = free.remove(0);
             Fox young = new Fox(false, field, loc);
             newFoxes.add(young);
         }
     }
-        
+
     /**
-     * Generate a number representing the number of births,
-     * if it can breed.
+     * Generate a number representing the number of births, if it can breed.
+     *
      * @return The number of births (may be zero).
      */
     private int breed()
     {
         int births = 0;
-        if(canBreed() && rand.nextDouble() <= BREEDING_PROBABILITY) {
+        if (canBreed() && rand.nextDouble() <= BREEDING_PROBABILITY)
+        {
             births = rand.nextInt(MAX_LITTER_SIZE) + 1;
         }
         return births;
@@ -179,4 +195,3 @@ public class Fox extends Actor
         return age >= BREEDING_AGE;
     }
 }
-
