f3350dfd20a1ba95f86f08a004a2021d454f95f7
HaIIvard <HaIIvard@158.38.193.235>
2017-02-09 13:07:06 +0100

    Added pregnancy period to rabbit

--- FoxesAndRabbitsSim/src/foxesandrabbitssim/Fox.java
+++ FoxesAndRabbitsSim/src/foxesandrabbitssim/Fox.java
@@ -29,7 +29,7 @@ public class Fox extends Animal
     private static final int MAX_LITTER_SIZE = 2;
     // The food value of a single rabbit. In effect, this is the
     // number of steps a fox can go before it has to eat again.
-    private static final int RABBIT_FOOD_VALUE = 9;
+    private static final int RABBIT_FOOD_VALUE = 20;
     // A shared random number generator to control breeding.
     private static final Random rand = Randomizer.getRandom();
 
