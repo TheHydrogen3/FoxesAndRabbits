1d531429fed3167777a61bc6bbcde377f01dc25d
HaIIvard <HaIIvard@158.38.193.235>
2017-02-15 09:16:14 +0100

    The Rabbits eat!!!

--- FoxesAndRabbitsSim/src/foxesandrabbitssim/Grass.java
+++ FoxesAndRabbitsSim/src/foxesandrabbitssim/Grass.java
@@ -3,52 +3,62 @@ package foxesandrabbitssim;
 import java.util.List;
 
 /**
- * A model of grass. Grass spreads every day in all directions depending on the growth rate
+ * A model of grass. Grass spreads every day in all directions depending on the
+ * growth rate
  *
  * @author Alexander Eilert Berg
  */
 public class Grass extends Actor
 {
+
     private static final int growthRate = 4;
-    
+
     /**
      * Creates the grass
+     *
      * @param field
-     * @param location 
+     * @param location
      */
     public Grass(Field field, Location location)
     {
-      super(field, location);
+        super(field, location);
     }
 
     /**
      * The grass grows
-     * @param newGrass 
+     *
+     * @param newGrass
      */
     @Override
     public void act(List<Actor> newGrass)
     {
-        if(isAlive())
+        if (isAlive())
         {
             grow(newGrass);
         }
     }
+
     /**
-     * The grass checks the area around itself, then grows on all the free spaces as many times as the growth rate allows.
-     * @param newGrass 
+     * The grass checks the area around itself, then grows on all the free
+     * spaces as many times as the growth rate allows.
+     *
+     * @param newGrass
      */
     protected void grow(List<Actor> newGrass)
     {
         Field field = getField();
         List<Location> free = field.getFreeAdjacentLocations(getLocation());
-        
+
         for (int c = 0; c <= Grass.growthRate; c++)
         {
-            if (free == null)
+            if (free != null)
             {
-                Location loc = free.remove(0);
-                Grass sprout = new Grass(field, loc);
-                newGrass.add(sprout);
+                Location loc = getField().freeAdjacentLocation(getLocation());
+                if (loc != null)
+                {
+                    Grass sprout = new Grass(field, loc);
+                    newGrass.add(sprout);
+                }
             }
         }
     }
